9bd03dde64603659a6aea67854318493c96f9159
Ryan Chang <rzinshanghai@gmail.com>
2020-06-01 02:18:36 -0400

    added a todo list adn planed out goals

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 //set up express app
 const app = express();
 
@@ -11,3 +11,9 @@ app.listen(process.env.port || 4000, () => {
 app.get("/express_backend", function (req, res) {
   res.send({ express: "YOUR BACKEND IS CONNECTED TO REACT" });
 });
+
+//TODO
+//1. Watch CSResouces/webdev routes
+//2. implement reddit veriosn of my news
+//3. learna how to serve js using express
+//4. lfie goals: podcast and famous movie cameo
